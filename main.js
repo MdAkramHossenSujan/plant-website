7ace313e3009e8f2223dfdabbefd9bb4acb0b486
akrmSujan <sujanban223@gmail.com>
2025-03-18 20:18:12 +0600

    first commit

--- main.js
+++ main.js
@@ -20,7 +20,44 @@ navLink.forEach(link=>{
 
 
 /*~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~*/
-
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+  speed:800,
+  spaceBetween:30,
+  autoplay:{
+delay:500,
+disableOnInteraction: false
+  },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      clickable:true
+    },
+    grabCursor:true,
+    breakpoints:{
+        640:{
+            slidesPerView:1
+        },
+        768:{
+            slidesPerView:2
+        },
+        1024:{
+            slidesPerView:3
+        }
+    }
+  });
 
 /*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
 
